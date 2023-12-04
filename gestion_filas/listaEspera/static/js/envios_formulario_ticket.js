9f8c195925485a00cc95643078f62e5006677017
Daniel Hernandez <daniher02@gmail.com>
2023-12-04 17:47:55 -0300

    se agrega nuevo mensaje de alerta

--- gestion_filas/listaEspera/static/js/envios_formulario_ticket.js
+++ gestion_filas/listaEspera/static/js/envios_formulario_ticket.js
@@ -41,22 +41,47 @@ $("#formLista_espera").submit(function (event) {
 
             if(response.ticket_espera !== null){
                 Swal.fire({
-                    title: `Tu número de atencion es: 
-                    <strong>${response.ticket_espera.centro}${response.ticket_espera.numero_espera}</strong>`,
-                    html: '¡Si ingresaste tu número telefónico, te llegará vía WhatsApp sino, te recomendamos <strong>Memorizarlo</strong>!',
-                    icon: 'success',
-                    confirmButtonText: 'Entendido',
+                    title: 'Tu turno será mostrado en la pantalla',
+                    html: `  <div class="alert alert-warning" role="alert">
+                                <i class="fa-regular fa-circle-exclamation"></i>
+                                <strong> No imprimimos papel </strong>
+                            </div>`,
+                    timer: 5000,  // Cierra el modal después de 5 segundos
+                    timerProgressBar: true,
+                    showConfirmButton: false,
+                    allowOutsideClick: false,
+                    iconHtml: '<i class="fa-solid fa-recycle fa-bounce" style="color: #1eb80a;"></i>',
                     customClass: {
-                        confirmButton: 'btn btn-success'
-                    },
-                    buttonsStyling: false,
-                    allowOutsideClick: false
-                }).then((result) => {
-                    // Después de hacer clic en "Volver", recargar la página
-                    if (result.isConfirmed) {
-                        location.reload();
+                        icon: 'icono-personalizado'
+                      },
+                    didClose: () => {
+                        Swal.fire({
+                            html: 'Puedes <strong>Memorizarlo</strong> o tómale una foto <i class="fa-regular fa-camera fa-fade" style="color: #892a7d;"></i>',
+                            iconHtml: `<h1 style="font-size: 1.5em;" >${response.ticket_espera.ticket}</h1>`,
+                            footer: '<span class="alert alert-warning" id="footer-text"></span>',
+                            customClass: {
+                                icon: 'icono-personalizado'
+                            },
+                            showConfirmButton: false,
+                            buttonsStyling: false,
+                            allowOutsideClick: false,
+                            timer: 20000,
+                            timerProgressBar: true,
+                            didOpen: () => {
+                                const footerText = document.querySelector('#footer-text');
+
+                                timerInterval = setInterval(() => {
+                                    let segundos = Math.floor(Swal.getTimerLeft() / 1000)
+                                    footerText.innerHTML = `<i class="fa-regular fa-clock"></i><b> ${segundos} segundos</b> para volver al inicio`
+                                }, 100)
+                              },
+                              willClose: () => {
+                                clearInterval(timerInterval)
+                                location.reload();
+                              }
+                        });
                     }
-                });
+                  });
             }else{
                 Swal.fire({
                     title: `¡Lo Siento!`,
