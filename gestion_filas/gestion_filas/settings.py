62e16cd1a60d8b3ae8e4df08a03ed0643803b186
Daniher01 <daniher02@gmail.com>
2024-11-14 16:34:34 -0300

    se prueba de manera local

--- gestion_filas/gestion_filas/settings.py
+++ gestion_filas/gestion_filas/settings.py
@@ -57,7 +57,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8000", 
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+    "http://gestionfilas.tuimagenlab.cl",
+    "https://gestionfilas.tuimagenlab.cl"
 ]
 
 CORS_ALLOW_METHODS = [
@@ -98,19 +101,19 @@ CHANNEL_LAYERS = {
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT', ''),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': os.environ.get('DB_NAME'),
+    #     'USER': os.environ.get('DB_USER'),
+    #     'PASSWORD': os.environ.get('DB_PASSWORD'),
+    #     'HOST': os.environ.get('DB_HOST'),
+    #     'PORT': os.environ.get('DB_PORT', ''),
+    # }
 }
 
 
